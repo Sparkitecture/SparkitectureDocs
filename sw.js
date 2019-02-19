39416391f26d222e0ccddcd6ba7f5b29f2ac483c
Matt <mshirk90@gmail.com>
2019-02-19 10:57:41 -0600

    updated icons

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
 
+//version: This comment initiates a cache refresh when changed
 self.addEventListener('fetch', function (event) {
  
     return;  
@@ -23,6 +24,8 @@ self.addEventListener('install', function (e) {
         '/SparkitectureDocs/Assets/Images/icon-96.png',
         '/SparkitectureDocs/Assets/Images/icon-144.png',
         '/SparkitectureDocs/Assets/Images/icon-192.png',
+        '/SparkitectureDocs/Assets/Images/logo-192.png',
+        '/SparkitectureDocs/Assets/Images/logo-512.png',
         '/SparkitectureDocs/Assets/Images/navbar-logo.png',
         '/SparkitectureDocs/Assets/Images/newapp.png',
         '/SparkitectureDocs/Assets/Images/OctocatHelp-sm.jpg',
