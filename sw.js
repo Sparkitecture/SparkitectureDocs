8dda9135514df733c10ab0052875adfb43febf87
Matt <mshirk90@gmail.com>
2019-02-25 12:26:27 -0600

    fixed if statement

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener('fetch', function (e) {
         return response || fetch(new Request("/SparkitectureDocs/offline.html"));
       })
     );
-  }else{
+  } else {
     e.respondWith(
       caches.match(e.request).then(function (response) {
         return response || fetch(e.request);
@@ -24,10 +24,11 @@ self.addEventListener('fetch', function (e) {
 
 self.addEventListener('install', function (e) {
   if (!navigator.onLine) {
+  } else {
     e.waitUntil(
       caches.open(cacheName).then(function (cache) {
         return cache.addAll([
-         // '/SparkitectureDocs/',
+          // '/SparkitectureDocs/',
           '/SparkitectureDocs/offline.html',
           '/SparkitectureDocs/index.html',
           '/SparkitectureDocs/Docs/mobile.html',
